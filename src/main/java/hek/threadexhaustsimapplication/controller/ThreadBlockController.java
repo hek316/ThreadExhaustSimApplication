80b7919386789229d7d1509c63053f9cb44b95e1
hek316 <yhr05008@naver.com>
2025-05-10 19:01:01 +0900

    "패키지 경로 오류 수정"

--- src/main/java/hek/threadexhaustsimapplication/controller/ThreadBlockController.java
+++ src/main/java/hek/threadexhaustsimapplication/controller/ThreadBlockController.java
@@ -1,4 +1,4 @@
-package hek.threadexhaustsim.controller;
+package hek.threadexhaustsimapplication.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
